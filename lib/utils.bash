8b28e64439cd9ddec8b690d52e241482c7af1548
Tim Colbert <s3than@users.noreply.github.com>
2024-05-30 11:46:22 +0000

    show alpha releases

--- lib/utils.bash
+++ lib/utils.bash
@@ -26,8 +26,7 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*$/\1/' |
-		grep -E '^[0-9]+\.[0-9]+\.[0-9]+$'
+		sed -E 's/^v([0-9]+\.[0-9]+\.[0-9]+).*$/\1/'
 }
 
 list_all_versions() {
