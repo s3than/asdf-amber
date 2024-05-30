cfe87bd9b925866b85bf3c1df9aac7b89e7d01c4
Tim Colbert <s3than@users.noreply.github.com>
2024-05-30 11:56:49 +0000

    fix executable

--- lib/utils.bash
+++ lib/utils.bash
@@ -85,8 +85,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		chmod +x "$install_path"
-
 		# TODO: Assert amber executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
